b7cc80aac4cca595c154df32f4ee27204ce17ad3
Nishtha <63602979+Nishtha0801@users.noreply.github.com>
2022-06-12 13:15:00 +0530

    Integrated the sunset API.

---
+++ script.js
@@ -0,0 +1,48 @@
+var predictButton = document.querySelector(".predict-two")
+console.log(predictButton);
+let optionOne = document.querySelector(".option-one");
+let optionTwo = document.querySelector(".option-two");
+let containerOne = document.querySelector(".section-container")
+let containerTwo = document.querySelector(".parttwo")
+
+optionOne.addEventListener("click", () => {
+    containerOne.scrollIntoView({behavior:"smooth"});
+})
+optionTwo.addEventListener("click", () => {
+    containerTwo.scrollIntoView({behavior:"smooth"});
+})
+
+function showPosition(position) {
+    console.log("Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude);
+    lat = position.coords.latitude;
+    long = position.coords.longitude;
+    var today = new Date();
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    console.log(date);
+    let text = document.querySelector(".text-two");
+    fetch(`https://geo-services-by-mvpc-com.p.rapidapi.com/sun_positions?location=${lat}%2C${long}&date=${date}`, options)
+        .then(response => response.json())
+        .then(response => text.innerHTML = `Sunset time is : ${response.data.sunset}`)
+        .catch(err => console.error(err));
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+}
+
+predictButton.addEventListener("click", () => {
+    getLocation();
+
+});
+
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'd7e287891bmshaee944ea936b106p1157a0jsn24be60ca2eb6',
+        'X-RapidAPI-Host': 'geo-services-by-mvpc-com.p.rapidapi.com'
+    }
+};
+
